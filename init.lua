2565e756cd795ab31e7295939f05591577363d3a
Nicolas Galler <ngaller@4gclinical.com>
2026-02-10 11:22:32 +0100

    Add mapping for Hyper-C (M-C on warp) + update rest of config to lua

--- init.lua
+++ init.lua
@@ -1,30 +1,20 @@
 require("config.lazy")
 
-vim.cmd([[
-	" Text, tab and indent related
-	"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-	set backspace=indent,eol,start
-	set expandtab
-	set shiftwidth=2
-	set tabstop=2
-	set smarttab
-
-	set linebreak   " break at sensible character
-	set tw=500
-
-	set autoindent
-	set nosmartindent
-	set wrap
-  set nu
-  set ignorecase smartcase
-]])
-
--- Highlight yanked text briefly
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
+-- Options
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smarttab = true
+vim.opt.linebreak = true
+vim.opt.textwidth = 500
+vim.opt.autoindent = true
+vim.opt.smartindent = false
+vim.opt.wrap = true
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.autowrite = true
 
 -- Persistent undo
 local undo_path = vim.fn.stdpath("data") .. "/undo"
@@ -33,5 +23,26 @@ if vim.fn.isdirectory(undo_path) == 0 then
 end
 vim.opt.undodir = undo_path
 vim.opt.undofile = true
--- write on :next
-vim.opt.autowrite = true
+
+-- Key mappings
+vim.keymap.set("i", "<M-C>", "<C-c>", { desc = "Hyper-C triggers Ctrl-C" , remap = true })
+vim.keymap.set({ "n", "v", "o" }, "<leader>cf", function()
+  vim.fn.setreg("+", vim.fn.expand("%"))
+end, { silent = true, desc = "Copy relative path to clipboard (src/foo.txt)" })
+vim.keymap.set("n", "<leader>cF", function()
+  vim.fn.setreg("*", vim.fn.expand("%:p"))
+end, { desc = "Copy absolute path to clipboard (/something/src/foo.txt)" })
+vim.keymap.set("n", "<leader>ct", function()
+  vim.fn.setreg("*", vim.fn.expand("%:t"))
+end, { desc = "Copy filename to clipboard (foo.txt)" })
+vim.keymap.set("n", "<leader>ch", function()
+  vim.fn.setreg("*", vim.fn.expand("%:p:h"))
+end, { desc = "Copy directory path to clipboard (/something/src)" })
+
+-- Autocommands
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight yanked text briefly",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
